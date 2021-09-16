b1a2b850396c081374d5c81a82190c2871328e89
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-09-16 22:03:47 +0000

    Updated the UI

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useDebounce from './useDebounce';
 
 export default function Counter() {
   const [value, setValue] = useState(0);
-  const lastValue = useDebounce(value, 500);
+  const lastValue = useDebounce(value, 1000);
   return (
     <div>
       <p>
-        Current: {value} - Debounced: {lastValue}
+        Normal value: {value} <span className="divider">|</span> Debounced value: {lastValue}
       </p>
       <button onClick={() => setValue(value + 1)}>Increment</button>
     </div>
